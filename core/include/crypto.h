bac2c5c0b6240734ae39087e95c9e452e80a8689
sleepyfran <fgzv@outlook.com>
2026-07-08 09:15:33 +0200

    Add required third party libs

---
+++ core/include/crypto.h
@@ -0,0 +1,17 @@
+#include <cstdint>
+#include <vector>
+
+namespace crypto
+{
+class CryptoProvider
+{
+  public:
+    virtual ~CryptoProvider() = default;
+
+    /**
+     * Generates the SHA256 of the given input using the operating system's
+     * crypto library.
+     */
+    virtual std::vector<uint8_t> sha256(std::string_view input) = 0;
+};
+} // namespace crypto
